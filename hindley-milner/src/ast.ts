719396d56832c28df6ee954e948993f117ebba08
Kevin Barabash <kevinb7@gmail.com>
2022-02-06 18:52:54 -0500

    format ast.ts

--- hindley-milner/src/ast.ts
+++ hindley-milner/src/ast.ts
@@ -6,134 +6,134 @@
 
 // TODO: update to support n-ary params
 export class Lambda {
-    params: string[];
-    body: Expression;
+  params: string[];
+  body: Expression;
 
-    constructor(params: string[] | string, body: Expression) {
-        this.params = Array.isArray(params) ? params : [params];
-        this.body = body;
-    }
+  constructor(params: string[] | string, body: Expression) {
+    this.params = Array.isArray(params) ? params : [params];
+    this.body = body;
+  }
 
-    toString(): string {
-        const {params, body} = this;
-        return `(fn ${params.map(p => p.toString()).join(" ")} => ${body})`;
-    }
+  toString(): string {
+    const { params, body } = this;
+    return `(fn ${params.map((p) => p.toString()).join(" ")} => ${body})`;
+  }
 }
 
 export class Identifier {
-    name: string;
+  name: string;
 
-    constructor(name: string) {
-        this.name = name;
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
 
-    toString(): string {
-        return this.name;
-    }
+  toString(): string {
+    return this.name;
+  }
 }
 
 export class Int {
-    value: number;
+  value: number;
 
-    constructor(value: number) {
-        this.value = value;
-    }
+  constructor(value: number) {
+    this.value = value;
+  }
 
-    toString(): string {
-        return this.value.toString();
-    }
+  toString(): string {
+    return this.value.toString();
+  }
 }
 
 export class Bool {
-    value: boolean;
+  value: boolean;
 
-    constructor(value: boolean) {
-        this.value = value;
-    }
+  constructor(value: boolean) {
+    this.value = value;
+  }
 
-    toString(): string {
-        return this.value.toString();
-    }
+  toString(): string {
+    return this.value.toString();
+  }
 }
 
 export class Arr {
-    // The AST allows for array literals to contain a mix of values.
-    // TODO: How will type inference handle a mix of values?
-    // - option 1: determine a type that encompases all types in the mix
-    // - option 2: disallow the mix unless we specify a type for the array
-    //             in the source, then check that this type works given the
-    //             values in the array
-    values: Expression[];
+  // The AST allows for array literals to contain a mix of values.
+  // TODO: How will type inference handle a mix of values?
+  // - option 1: determine a type that encompases all types in the mix
+  // - option 2: disallow the mix unless we specify a type for the array
+  //             in the source, then check that this type works given the
+  //             values in the array
+  values: Expression[];
 
-    constructor(values: Expression[]) {
-        this.values = values;
-    }
+  constructor(values: Expression[]) {
+    this.values = values;
+  }
 
-    toString(): string {
-        return `[${this.values.map(v => v.toString()).join(", ")}]`;
-    }
+  toString(): string {
+    return `[${this.values.map((v) => v.toString()).join(", ")}]`;
+  }
 }
 
 export class Literal {
-    value: Int | Bool | Arr;
+  value: Int | Bool | Arr;
 
-    constructor(value: Int | Bool | Arr) {
-        this.value = value;
-    }
+  constructor(value: Int | Bool | Arr) {
+    this.value = value;
+  }
 
-    toString(): string {
-        return this.value.toString();
-    }
+  toString(): string {
+    return this.value.toString();
+  }
 }
 
 // TODO: update to support n-ary args
 export class Apply {
-    fn: Expression;
-    args: Expression[];
+  fn: Expression;
+  args: Expression[];
 
-    constructor(fn: Expression, args: Expression[] | Expression) {
-        this.fn = fn;
-        this.args = Array.isArray(args) ? args : [args];
-    }
+  constructor(fn: Expression, args: Expression[] | Expression) {
+    this.fn = fn;
+    this.args = Array.isArray(args) ? args : [args];
+  }
 
-    toString(): string {
-        const {fn, args} = this;
-        return `(${fn} ${args.map(a => a.toString()).join(" ")})`;
-    }
+  toString(): string {
+    const { fn, args } = this;
+    return `(${fn} ${args.map((a) => a.toString()).join(" ")})`;
+  }
 }
 
 export class Let {
-    v: string;
-    defn: Expression;
-    body: Expression;
+  v: string;
+  defn: Expression;
+  body: Expression;
 
-    constructor(v: string, defn: Expression, body: Expression) {
-        this.v = v;
-        this.defn = defn;
-        this.body = body;
-    }
+  constructor(v: string, defn: Expression, body: Expression) {
+    this.v = v;
+    this.defn = defn;
+    this.body = body;
+  }
 
-    toString() {
-        const {v, defn, body} = this;
-        return `(let ${v} = ${defn} in ${body})`;
-    }
+  toString() {
+    const { v, defn, body } = this;
+    return `(let ${v} = ${defn} in ${body})`;
+  }
 }
 
 export class Letrec {
-    v: string;
-    defn: Expression;
-    body: Expression;
+  v: string;
+  defn: Expression;
+  body: Expression;
 
-    constructor(v: string, defn: Expression, body: Expression) {
-        this.v = v;
-        this.defn = defn;
-        this.body = body;
-    }
+  constructor(v: string, defn: Expression, body: Expression) {
+    this.v = v;
+    this.defn = defn;
+    this.body = body;
+  }
 
-    toString() {
-        const {v, defn, body} = this;
-        return `(letrec ${v} = ${defn} in ${body})`;
-    }
+  toString() {
+    const { v, defn, body } = this;
+    return `(letrec ${v} = ${defn} in ${body})`;
+  }
 }
 
 export type Expression = Lambda | Identifier | Apply | Let | Letrec | Literal;
