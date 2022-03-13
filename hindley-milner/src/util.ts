a5bfdc88e74307329e5df9d1886c18a5bb543a95
Kevin Barabash <kevinb7@gmail.com>
2022-03-13 19:04:44 -0400

    Delete hindley-milner directory

--- hindley-milner/src/util.ts
+++
@@ -1,8 +0,0 @@
-export const zip = <T>(xs: T[], ys: T[]): [T, T][] => {
-  const length = Math.min(xs.length, ys.length);
-  const result: [T, T][] = [];
-  for (let i = 0; i < length; i++) {
-    result[i] = [xs[i], ys[i]];
-  }
-  return result;
-}
